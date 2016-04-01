6593f49bb70559af859395e41bf363f999882add
Jason Melton <jason.melton@gmail.com>
2016-04-01 07:33:55 -0400

    Schema and seed.

--- database/schema.sql
+++ database/schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS  clerks (
-   id          INT                      NOT NULL AUTO_INCREMENT,
+   clerk_id          INT                      NOT NULL AUTO_INCREMENT,
    login       VARCHAR(16)              NOT NULL,
    first_name  VARCHAR(45)              NOT NULL,
    last_name   VARCHAR(45)              NOT NULL,
    password    VARCHAR(32)              NOT NULL,
-   PRIMARY KEY (id),
+   PRIMARY KEY (clerk_id),
    UNIQUE      (login));
 
 CREATE TABLE IF NOT EXISTS customers(
-    id                  INT             NOT NULL AUTO_INCREMENT,
+    customer_id                  INT             NOT NULL AUTO_INCREMENT,
     email               VARCHAR(255)    NOT NULL,
     first_name          VARCHAR(45)     NOT NULL,
     last_name           VARCHAR(45)     NOT NULL,
@@ -18,11 +18,11 @@ CREATE TABLE IF NOT EXISTS customers(
     work_phone_number   VARCHAR(255)    NOT NULL,
     home_phone_cc       VARCHAR(4)      NOT NULL,
     home_phone_number   VARCHAR(255)    NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (customer_id),
     UNIQUE      (email));
     
 CREATE TABLE IF NOT EXISTS reservations(
-    id                  INT             NOT NULL,
+    reservation_id                  INT             NOT NULL AUTO_INCREMENT,
     start_date          DATE            NOT NULL,
     end_date            DATE            NOT NULL,
     customer_id         INT             NOT NULL,
@@ -30,60 +30,60 @@ CREATE TABLE IF NOT EXISTS reservations(
     clerk_id_dropoff    INT,
     credit_card         VARCHAR(45),
     expiration_date     DATE,
-    PRIMARY KEY (id),
+    PRIMARY KEY (reservation_id),
     UNIQUE (customer_id, credit_card),
-    FOREIGN KEY (customer_id)           REFERENCES customers (id),
-    FOREIGN KEY (clerk_id_pickup)       REFERENCES clerks (id),
-    FOREIGN KEY (clerk_id_dropoff)      REFERENCES clerks (id) );
+    FOREIGN KEY (customer_id)           REFERENCES customers (customer_id),
+    FOREIGN KEY (clerk_id_pickup)       REFERENCES clerks (clerk_id),
+    FOREIGN KEY (clerk_id_dropoff)      REFERENCES clerks (clerk_id) );
     
 CREATE TABLE IF NOT EXISTS categories(
-    id                  INT             NOT NULL,
+    category_id                  INT             NOT NULL AUTO_INCREMENT,
     category            VARCHAR(255)    NOT NULL,
     has_accessories     BOOLEAN         NOT NULL,
-    PRIMARY KEY (id), 
+    PRIMARY KEY (category_id), 
     UNIQUE (category) );
 
 CREATE TABLE IF NOT EXISTS tools( 
-    id                  INT             NOT NULL,
+    tool_id                  INT             NOT NULL AUTO_INCREMENT,
     short_description   VARCHAR(255)    NOT NULL,
     full_description    TEXT(500)       NOT NULL,
-    deposit             DECIMAL(5,2)    NOT NULL,
-    day_price           DECIMAL(5,2)    NOT NULL,
-    original_price      DECIMAL(5,2)    NOT NULL,
+    deposit             DECIMAL(7,2)    NOT NULL,
+    day_price           DECIMAL(7,2)    NOT NULL,
+    original_price      DECIMAL(7,2)    NOT NULL,
     category_id         INT             NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (category_id)           REFERENCES categories (id) );
+    PRIMARY KEY (tool_id),
+    FOREIGN KEY (category_id)           REFERENCES categories (category_id) );
 
 CREATE TABLE IF NOT EXISTS tool_accessories(
     tool_id             INT             NOT NULL,
     description         VARCHAR(255)    NOT NULL,
     PRIMARY KEY (tool_id, description),
-    FOREIGN KEY (tool_id)               REFERENCES tools (id) );
+    FOREIGN KEY (tool_id)               REFERENCES tools (tool_id) );
 
 CREATE TABLE IF NOT EXISTS reservations_tools(
     reservation_id      INT             NOT NULL,
     tool_id             INT             NOT NULL,
     PRIMARY KEY (reservation_id, tool_id),
-    FOREIGN KEY (reservation_id)        REFERENCES reservations (id),
-    FOREIGN KEY (tool_id)               REFERENCES tools (id) );
+    FOREIGN KEY (reservation_id)        REFERENCES reservations (reservation_id),
+    FOREIGN KEY (tool_id)               REFERENCES tools (tool_id) );
 
 CREATE TABLE IF NOT EXISTS service_orders(
-    id                  INT             NOT NULL,
+    service_order_id                  INT             NOT NULL AUTO_INCREMENT,
     clerk_id            INT             NOT NULL,
     tool_id             INT             NOT NULL,
     start_date          DATE            NOT NULL,
     end_date            DATE,
-    est_cost            DECIMAL(5,2),
-    PRIMARY KEY (id),
-    FOREIGN KEY (clerk_id)              REFERENCES clerks (id),
-    FOREIGN KEY (tool_id)               REFERENCES tools (id) );
+    est_cost            DECIMAL(7,2),
+    PRIMARY KEY (service_order_id),
+    FOREIGN KEY (clerk_id)              REFERENCES clerks (clerk_id),
+    FOREIGN KEY (tool_id)               REFERENCES tools (tool_id) );
 
 CREATE TABLE IF NOT EXISTS sells(
     tool_id             INT             NOT NULL,
     clerk_id            INT             NOT NULL,
     sale_date           DATE            NOT NULL,
     PRIMARY KEY (tool_id),
-    FOREIGN KEY (clerk_id)              REFERENCES clerks (id),
-    FOREIGN KEY (tool_id)               REFERENCES tools (id) );
+    FOREIGN KEY (clerk_id)              REFERENCES clerks (clerk_id),
+    FOREIGN KEY (tool_id)               REFERENCES tools (tool_id) );
 
 
