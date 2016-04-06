2c90d9e77338e5d0a0141401aec7a618cec5d0e4
Jay Rutten <jrutten@pacbell.net>
2016-04-05 23:55:24 -0700

    Made use of database.json in about page.

--- controllers/about.py
+++ controllers/about.py
@@ -5,13 +5,9 @@ import pymysql.cursors
 import os
 import json
 
-connection = pymysql.connect(host='localhost',
-                             db='handymandb',
-                             charset='utf8',
-                             cursorclass=pymysql.cursors.DictCursor)
-
 # Specify config in database.json or editing 'database' variable below
 db_config_file = os.path.join(os.path.dirname(__file__), "database.json")
+print(db_config_file)
 
 if os.path.exists(db_config_file):
     with open(db_config_file) as f:
@@ -20,12 +16,19 @@ else:
         database = [ {"host":"localhost", "db":"test", "user":"root", "passwd":""}]
 
 # Establish Databse Connection
-connection = []
-for param in database:
-    connection.append(pymysql.connect(**param))
+#connection = []
+#for param in database:
+#    connection.append(pymysql.connect(**param))
+
+connection = pymysql.connect(host=database[0]['host'],
+                             user=database[0]['user'],
+                             passwd=database[0]['passwd'],
+                             db=database[0]['db'],
+                             charset='utf8',
+                             cursorclass=pymysql.cursors.DictCursor)
+
 
 cursor = connection.cursor()
-
 
 @route('/about')
 @view('about')
