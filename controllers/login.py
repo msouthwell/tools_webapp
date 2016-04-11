7e18650cff6468864cb61cc144844794fc8c0161
Addison Huddy <addisonhuddy@gmail.com>
2016-04-11 13:44:13 -0700

    login refactor working. Next going to stub out a page for clerks home screen

--- controllers/login.py
+++ controllers/login.py
@@ -3,6 +3,7 @@ import pymysql.cursors
 import pymysql.err
 import os
 import json
+from database import dbapi
 
 
 @route('/login', method=['GET', 'POST'])
@@ -13,41 +14,24 @@ def login():
         password = request.forms.get('password', '').strip()
         user_type = request.forms.get('usertype', '').strip()
 
-        #TODO refactor this out into a seperate datbase class
-        try:
-            # Specify config in database.json or editing 'database' variable below
-            db_config_file = os.path.join(os.path.dirname(__file__), "database.json")
+        connection = dbapi.connect()  # return db connection
+        if connection == -1:
+            return template('login.tpl')
 
-            if os.path.exists(db_config_file):
-                with open(db_config_file) as f:
-                    database = json.load(f)
-            else:
-                    database = [ {"host":"localhost", "db":"test", "user":"root", "passwd":""}]
+        if user_type == "clerks":
+            sql = "SELECT clerk_id, login, password FROM clerks WHERE login=%s"
+        elif user_type == "customers":
+            sql = "SELECT customer_id, email, password FROM customers WHERE email=%s"
 
-            connection = pymysql.connect(host=database[0]['host'],
-                                         user=database[0]['user'],
-                                         passwd=database[0]['passwd'],
-                                         db=database[0]['db'],
-                                         charset='utf8',
-                                         cursorclass=pymysql.cursors.DictCursor)
+        print(sql)
+        c = connection.cursor()
+        c.execute(sql, login)  # login or email
+        result = c.fetchone()
 
-            c = connection.cursor()
-            if user_type == "clerks":
-                sql = "SELECT clerk_id, login, password FROM clerks WHERE login=%s"
-            elif user_type == "customers":
-                sql = "SELECT customer_id, email, password FROM customers WHERE email=%s"
-            c.execute(sql, login)  # login or email
+        if result is None:
+            return template('login.tpl', message='Email or password incorrect')
 
-            result = c.fetchone()
-
-            if result is None:
-                return template('login.tpl', message='Email or password incorrect')
-
-            connection.commit() # why commit?
-            c.close()
-
-        except pymysql.err.Error as e:
-            return template('login.tpl', message='An error occurred. Error {!r}, errno is {}'.format(e, e.args[0]))
+        c.close()
 
         if password == result['password']:
             if user_type == "clerks":
