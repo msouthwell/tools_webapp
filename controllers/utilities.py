3fd0a94317d8882007877b81f0e58c37d71ff57e
msouth6 <matthewsouthwell13@gmail.com>
2016-04-14 23:10:03 +0900

    Added reservation_tools and date_differance methods

--- controllers/utilities.py
+++ controllers/utilities.py
@@ -1,6 +1,10 @@
 
 from bottle import template
+import pymysql.cursors
+import pymysql.err
+from database import dbapi
 
+# returns the reservation data
 def view_reservation(reservation_id):
 
     try:
@@ -12,9 +16,44 @@ def view_reservation(reservation_id):
 
         sql = "SELECT * FROM RESERVATIONS " \
             "WHERE reservation_id = %s"
-        print(sql, reservation_id)
         c.execute(sql, reservation_id)
         data= c.fetchone()
+        c.close()
+    except pymysql.err.Error as e:
+        return template('error.tpl', message='An error occurred. Error {!r}, errno is {}'.format(e, e.args[0]))
+    else:
+        return data
+
+# there is likely a way simpler and more elegant way to do this, but it works for now
+def date_differance(start_date, end_date):
+    try:
+        connection = dbapi.connect()
+        if connection == -1:
+            return template('error.tpl', message="Database could not connect");
+
+        c = connection.cursor()
+        dd = "SELECT DATEDIFF (%s, %s) AS Diffdate"
+        c.execute(dd, (str(end_date), str(start_date)))
+        datediff = c.fetchone()
+        c.close()
+    except pymysql.err.Error as e:
+        return template('error.tpl', message='An error occurred. Error {!r}, errno is {}'.format(e, e.args[0]))
+    else:
+        return datediff['Diffdate']
+
+def reservation_tools(reservation_id):
+
+    try:
+        connection = dbapi.connect()
+        if connection == -1:
+            return template('error.tpl', message="Database could not connect");
+
+        c = connection.cursor()
+
+        sql = "SELECT * FROM RESERVATIONS_TOOLS NATURAL JOIN TOOLS " \
+            "WHERE reservation_id = %s"
+        c.execute(sql, reservation_id)
+        data= c.fetchall()
         c.close()
     except pymysql.err.Error as e:
         return template('error.tpl', message='An error occurred. Error {!r}, errno is {}'.format(e, e.args[0]))
